bb6b834ebb5547cba13118bd8f4aba7195623013
srplanta <srplanta@gmail.com>
2023-03-06 23:40:59 +0500

    jQuery applied on Add View of FeeTransactionController. A logical error found in [HttpPost] Add method, not evaluating blank student id.

--- StudentManagementSystem/Controllers/FeeTransactionsController.cs
+++ StudentManagementSystem/Controllers/FeeTransactionsController.cs
@@ -22,30 +22,37 @@ namespace StudentManagementSystem.Controllers
             return View(feeTransactions);
         }
 
-        public IActionResult Add(int? id)
+        [HttpGet]
+        public IActionResult Add(int? id)   //id => studentId
         {
             ViewBag.StudentId = new SelectList(_context.Students, "StdId", "Name");
 
-
             if (id > 0)
             {
+                //fetch previous unpaid fee bill if any?
                 var previousFeeTransaction = _context.FeeTransactions
                     .FirstOrDefault(f => f.StudentId == id && f.FeePaid == null);
                 if (previousFeeTransaction != null)
                 {
+                    //If previous unpaid fee bill found, make view form filled ready from previous record
+                    //Database record will not be affected with this
+                    //Following previousFeeTransaction.PreviousArrears are previousFeeTransaction.NextArrears in actual but ...
+                    //will be used as newFeeTransaction.PreviousArrears in view form.
+                    //Actual database operation will be handled in [HttpPost] Add(FeeTransaction newFeeTransaction) method.
                     previousFeeTransaction.PreviousArrears = previousFeeTransaction.FeePayable;
                     previousFeeTransaction.AdmissionFee = null;
                     previousFeeTransaction.FeePaid = null;
                     previousFeeTransaction.FeePayable = null;
-                    //previousFeeTransaction.TutionFee = 0m;
                     previousFeeTransaction.Fine = null;
                     previousFeeTransaction.NextArrears = null;
                     previousFeeTransaction.StationaryCharges = null;
+                    previousFeeTransaction.Student = _context.Students.Find(id) ?? new Student();
                 }
                 return View(previousFeeTransaction);
             }
             else
             {
+                //If previous unpaid/unsettelled fee bill not found, generate a new blank form
                 return View();
             }
         }
@@ -53,11 +60,12 @@ namespace StudentManagementSystem.Controllers
         [HttpPost]
         public IActionResult Add(FeeTransaction newFeeTransaction)
         {
-            if (newFeeTransaction.TutionFee > 0)
+            if (newFeeTransaction.TutionFee > 0)    //New fee bill requested?
             {
-                //Make previous fee bill read only
+                //fetch previous unpaid fee bill if any?
                 var previousFeeTransaction = _context.FeeTransactions
                     .FirstOrDefault(f => f.StudentId == newFeeTransaction.StudentId && f.FeePaid == null);
+                //Make previous fee bill read only
                 if (previousFeeTransaction != null && previousFeeTransaction.FeePaid == null)
                 {
                     previousFeeTransaction.FeePaid = 0;
@@ -66,7 +74,7 @@ namespace StudentManagementSystem.Controllers
                     _context.SaveChanges();
 
                     //newFeeTransaction.PreviousArrears = previousFeeTransaction.NextArrears;
-                    newFeeTransaction.FeeId = 0;
+                    //newFeeTransaction.FeeId = 0;
                     //TempData["Message"] = $"INFO: Fee Transaction against ID#{previousFeeTransaction.FeeId} is made read only.";
                 }
                 //Generate new fee bill in context of previous fee bill
@@ -75,11 +83,12 @@ namespace StudentManagementSystem.Controllers
                 newFeeTransaction.AdmissionFee ??= 0m;
                 newFeeTransaction.Fine ??= 0m;
                 newFeeTransaction.StationaryCharges ??= 0m;
-                newFeeTransaction.FeePayable = newFeeTransaction.TutionFee
+                newFeeTransaction.FeePayable ??= newFeeTransaction.TutionFee
                     + newFeeTransaction.PreviousArrears
                     + newFeeTransaction.AdmissionFee
                     + newFeeTransaction.Fine
                     + newFeeTransaction.StationaryCharges;
+                newFeeTransaction.Student = _context.Students.Find(newFeeTransaction.StudentId) ?? new Student();
                 _context.FeeTransactions.Add(newFeeTransaction);
                 _context.SaveChanges();
 
@@ -97,8 +106,9 @@ namespace StudentManagementSystem.Controllers
             }
         }
 
-        public IActionResult Edit(int? id)
+        public IActionResult Edit(int? id)  //id => FeeId
         {
+            ViewBag.StudentId = new SelectList(_context.Students, "StdId", "Name");
             if (id == null || _context.FeeTransactions == null)
             {
                 return NotFound();
@@ -106,6 +116,15 @@ namespace StudentManagementSystem.Controllers
             else
             {
                 var feeTransaction = _context.FeeTransactions.Find(id);
+                if ((feeTransaction?.FeePaid) != null)
+                {
+                    TempData["FeePaid"] = true;
+                    TempData["Message"] = "INFO: This fee bill is already setteled down and cannot be edited.";
+                }
+                else
+                {
+                    TempData["FeePaid"] = false;
+                }
                 return View(feeTransaction);
             }
         }
